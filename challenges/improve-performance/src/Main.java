7e1be4ce452bbaea911cc2a4b9c67845c216ebb6
Isaiah <isaiahco2022@gmail.com>
2026-09-11 16:11:48 +0000

    opt: optimized prime calculations

--- challenges/improve-performance/src/Main.java
+++ challenges/improve-performance/src/Main.java
@@ -1,6 +1,7 @@
 import java.math.BigInteger;
 import java.util.List;
 
+
 public class Main {
     public static void main(String[] args) {
 
--- challenges/improve-performance/src/PrimeGenerator.java
+++ challenges/improve-performance/src/PrimeGenerator.java
@@ -1,21 +1,27 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PrimeGenerator {
 
     public List<BigInteger> getPrimes(int size) {
 
-        System.out.println("About to find " + size + " primes.");
+    System.out.println("About to find " + size + " primes.");
 
-       List<BigInteger> primes = new ArrayList<>();
-        
-        for (int i = 0; i < size; i++) {
-            primes.add(new BigInteger(2000, new Random()).nextProbablePrime());
-        }
+    List<BigInteger> primes = IntStream.range(0, size)
+        .parallel()
+        .mapToObj(i ->
+            new BigInteger(
+                2000,
+                16,
+                ThreadLocalRandom.current())
+        )
+        .toList();
 
-        System.out.println("Found all " + primes.size() + " primes.");
-        return primes;          
-    }
+    System.out.println("Found all " + primes.size() + " primes.");
+
+    return primes;
+}
 }
